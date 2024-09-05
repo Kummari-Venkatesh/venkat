4cb322c68c5c6f75f175df34146d7153e425754f
venkatesh <vkvenkatesh951@gmail.com>
2024-09-05 18:01:22 +0530

    commiting Demo

---
+++ Dam.java
@@ -0,0 +1,11 @@
+class Dam
+{
+static int a,b,c;
+public static void main(String args[])
+{
+a=Integer.parseInt(args[0]);
+b=Integer.parseInt(args[1]);
+c=a+b;
+System.out.println("sum of two numbers is="+c);
+}
+}
